38a00c253b19c3e0cc5f8ffedad9cb3c3fca9d49
ariaandika <andikaakun17@gmail.com>
2023-12-20 13:05:05 +0700

    server: add proxy forwarding

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,5 @@
 
-use std::{sync::{Arc, RwLock}, fmt::{Debug, Display}, time::Duration};
+use std::{sync::{Arc, RwLock}, fmt::{Debug, Display}, collections::HashMap};
 
 use http::{Request, Response, StatusCode};
 use http_body_util::{combinators::BoxBody, BodyExt, Full};
@@ -13,16 +13,31 @@ async fn main() {
 
 static SERVICE_UNAVAILABLE: &[u8] = b"Service Unavailable";
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 struct AppState {
-    #[allow(dead_code)]
-    shutdown: bool
+    domains: HashMap<String,DomainConfig>
+}
+
+impl Default for AppState {
+    fn default() -> Self {
+        let mut map = HashMap::new();
+        map.insert("localhost:3000".into(), DomainConfig { target: "localhost:8000".into() });
+        map.insert("localhost:4000".into(), DomainConfig { target: "localhost:4000".into() });
+        Self {
+            domains: map
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+struct DomainConfig {
+    target: String,
 }
 
 async fn server<T: ToSocketAddrs>(addr: T) -> Result<(), std::io::Error> {
     let tcp = TcpListener::bind(addr).await?;
 
-    let config = Arc::new(RwLock::new(AppState { shutdown: false }));    
+    let config = Arc::new(RwLock::new(AppState::default()));    
 
     signal_handler(config.clone());
 
@@ -33,9 +48,14 @@ async fn server<T: ToSocketAddrs>(addr: T) -> Result<(), std::io::Error> {
         };
 
         let io = hyper_util::rt::TokioIo::new(stream);
-
+        let config = Arc::clone(&config);
         let server = hyper::server::conn::http1::Builder::new()
-                .serve_connection(io, service_fn(dummy_handle));
+                .serve_connection(io, service_fn(move |req| {
+                    let config = Arc::clone(&config);
+                    async move {
+                        dummy_handle(req, config).await
+                    }
+                }));
 
         tokio::spawn(async move {
             if let Err(err) = server.await {
@@ -46,18 +66,40 @@ async fn server<T: ToSocketAddrs>(addr: T) -> Result<(), std::io::Error> {
     Ok(())
 }
 
-async fn dummy_handle(req: Request<Incoming>) -> Result<Response<BoxBody<Bytes, hyper::Error>>, std::io::Error> {
-    let proxy_addr = "127.0.0.1:8000";
-    let tcp = TcpStream::connect(proxy_addr).await;
+async fn dummy_handle(req: Request<Incoming>, config: Arc<RwLock<AppState>>) -> Result<Response<BoxBody<Bytes, hyper::Error>>, std::io::Error> {
+
+    let host = match req.headers().get("host") {
+        Some(host) => match host.to_str() {
+            Ok(h) => h,
+            Err(_) => return bad_request(),
+        },
+        None => return bad_request()
+    };
+
+    let proxy_addr = {
+        let target = config.read().expect("Deadlock");
+        match target.domains.get(host) {
+            Some(c) => {
+                let t = &c.target;
+                t.clone()
+            }
+            None => return not_found()
+        }
+    };
+
+    let tcp = TcpStream::connect(&proxy_addr).await;
 
     let tcp = match tcp {
         Ok(t) => t,
-        Err(err) => return service_unavailable(proxy_addr, err)
+        Err(err) => return service_unavailable(&proxy_addr, err)
     };
 
     let io = hyper_util::rt::TokioIo::new(tcp);
 
-    let (mut sender, conn): (SendRequest<Incoming>,_) = hyper::client::conn::http1::handshake(io).await.unwrap();
+    let (mut sender, conn): (SendRequest<Incoming>,_) = match hyper::client::conn::http1::handshake(io).await {
+        Ok(c) => c,
+        Err(err) => return service_unavailable(proxy_addr, err),
+    };
 
     tokio::spawn(async move {
         if let Err(err) = conn.await {
@@ -79,6 +121,22 @@ fn service_unavailable<T: Display, Err: Display>(proxy_addr: T, err: Err) -> Res
     let res: Response<BoxBody<Bytes, hyper::Error>> = Response::builder()
         .status(StatusCode::SERVICE_UNAVAILABLE)
         .body(Full::new(SERVICE_UNAVAILABLE.into()).map_err(|e|match e {}).boxed())
+        .unwrap();
+    Ok(res)
+}
+
+fn bad_request() -> Result<Response<BoxBody<Bytes, hyper::Error>>, std::io::Error> {
+    let res: Response<BoxBody<Bytes, hyper::Error>> = Response::builder()
+        .status(StatusCode::BAD_REQUEST)
+        .body(Full::new(Bytes::default()).map_err(|e|match e {}).boxed())
+        .unwrap();
+    Ok(res)
+}
+
+fn not_found() -> Result<Response<BoxBody<Bytes, hyper::Error>>, std::io::Error> {
+    let res: Response<BoxBody<Bytes, hyper::Error>> = Response::builder()
+        .status(StatusCode::NOT_FOUND)
+        .body(Full::new(Bytes::default()).map_err(|e|match e {}).boxed())
         .unwrap();
     Ok(res)
 }
